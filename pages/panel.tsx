c8d23a28999b461e24e7392e7c5adb36ce3009a7
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-07-28 12:01:00 -0500

    sidebar working, starting funcionality

--- pages/panel.tsx
+++ pages/panel.tsx
@@ -12,11 +12,14 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 //drawer
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -54,10 +57,41 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+function ResponsiveDrawer() {
+  const classes = useStyles();
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const handleListItemClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
+  const listItems = ["Clients", "Campaigns", "Assosiates", "Personal Stats"];
+
+  return (
+    <div>
+      <div className={classes.toolbar}>
+        <List component="nav" aria-label={listItems.join(" ")}>
+          {listItems.map((text, index) => (
+            <ListItem
+              button
+              key={text + index}
+              selected={selectedIndex === index}
+              onClick={() => handleListItemClick(index)}
+            >
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </div>
+  );
+}
+
 function Panel() {
   const classes = useStyles();
   const theme = useTheme();
   const [drawerOpen, setDrawerOpen] = React.useState(false);
+  //const [activeSection, setActiveSection] = React.useState(0)
 
   const handleDrawerToggle = () => {
     setDrawerOpen((prev) => !prev);
@@ -72,40 +106,50 @@ function Panel() {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            className={classes.menuutton}
+            className={classes.menuButton}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">Campaigns Dashboard</Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        {/* Open on demand variant */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            // container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={drawerOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            <ResponsiveDrawer />
+          </Drawer>
+        </Hidden>
+        {/* Always open variant */}
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            <ResponsiveDrawer />
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main>content here</main>
     </div>
   );
 }
 
 export default Panel;
-
-function ResonsiveDrawer({ isOpen }) {
-  const classes = useStyles();
-
-  const [selectedIndex, setSelectedIndex] = React.useState(undefined);
-  const handleListItemClick = (index: number) => {
-    setSelectedIndex(index);
-  };
-
-  const listItems = ["Clients", "Campaigns", "Assosiates", "Personal Stats"];
-
-  return (
-    <div>
-      <div className={classes.toolbar}>
-        <List>
-          {listItems.map((text, index) => (
-            <ListItem button key={text + index}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    </div>
-  );
-}
