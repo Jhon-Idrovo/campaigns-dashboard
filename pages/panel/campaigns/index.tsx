d832a3b5d35140eceeaef79c1263ddee73ed462d
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-07-28 14:33:51 -0500

    folder routing added

---
+++ pages/panel/campaigns/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../../src/components/Layout";
+
+function Index() {
+  return (
+    <Layout>
+      kjljlksdjf
+      <div>dfdfs</div>
+    </Layout>
+  );
+}
+
+export default Index;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles
+} from "@material-ui/core/styles";
+
+import AppBar from "@material-ui/core/AppBar";
+
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+//drawer
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { useRouter } from "next/router";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0
+      }
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth
+      }
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none"
+      }
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
+
+function DrawerContent() {
+  const classes = useStyles();
+  const router = useRouter();
+
+  const listItems = [
+    { text: "Clients", href: "clients" },
+    { text: "Campaigns", href: "campaigns" },
+    { text: "Associates", href: "associates" },
+    { text: "Personal Stats", href: "stats" }
+  ];
+
+  return (
+    <div>
+      <div className={classes.toolbar}>
+        <List component="nav" aria-label={listItems.join(" ")}>
+          {listItems.map(({ text, href }, index) => (
+            <ListItem
+              button
+              key={text + index}
+              selected={router.pathname.includes(href)}
+              onClick={() => router.push(href)}
+            >
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </div>
+  );
+}
+
+function Panel({ children }) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Campaigns Dashboard</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        {/* Open on demand variant */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            // container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={drawerOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            <DrawerContent />
+          </Drawer>
+        </Hidden>
+        {/* Always open variant */}
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            <DrawerContent />
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main>
+        <div className={classes.toolbar} />
+        <Typography color="primary">jlkjlj</Typography>
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Panel;
