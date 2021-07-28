95ef2a497f4ea4c392ab5b5f367d7c0b697f4ae0
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-07-28 09:31:34 -0500

    first commit

--- pages/panel.tsx
+++ pages/panel.tsx
@@ -1,19 +1,116 @@
+import React from "react";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles
+} from "@material-ui/core/styles";
+
+import AppBar from "@material-ui/core/AppBar";
+
+import IconButton from "@material-ui/icons/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+//drawer
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0
+      }
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth
+      }
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none"
+      }
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
+
 function Panel() {
-  return <div>testing</div>;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prev) => !prev);
+  };
+  //Drawer
+  const [selectedIndex, setSelectedIndex] = React.useState(undefined);
+  const handleListItemClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
+  const listItems = ["Clients", "Campaigns", "Assosiates", "Personal Stats"];
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar}>
+        <List>
+          {listItems.map((text, index) => (
+            <ListItem button key={text + index}>
+              {" "}
+              <ListItemText primary={text} />{" "}
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuutton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Campaigns Dashboard</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Panel;
 
-const sideBarItems = ["Clients", "Campaigns", "Assosiates", "Personal Stats"];
-
 function SideBar() {
   return (
     <div>
-      <ul>
-        {sideBarItems.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <ul></ul>
     </div>
   );
 }
