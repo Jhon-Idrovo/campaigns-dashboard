6f91009eba978e29103e99587a134834693add58
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-08-01 08:44:24 -0500

    Sorting working both, up and down

--- pages/panel/campaigns/index.tsx
+++ pages/panel/campaigns/index.tsx
@@ -1,117 +1,17 @@
+import CampaignsTableBody from "../../../src/components/CampaignsTableBody";
 import Layout from "../../../src/components/Layout";
-import {
-  DataGrid,
-  GridColDef,
-  GridValueGetterParams,
-} from "@material-ui/data-grid";
+import EnhancedTable from "../../../src/components/Table";
 
 import useCampaigns from "../../../src/hooks/useCampaigns";
 function Index() {
-  const rows = useCampaigns();
-  const columns: GridColDef[] = [
-    {
-      field: "id",
-      headerName: "ID",
-      width: 90,
-    },
-    {
-      field: "pages",
-      headerName: "Pages",
-      type: "number",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 130,
-      editable: true,
-    },
-    {
-      field: "impressions",
-      type: "number",
-      headerName: "Impressions",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 170,
-      editable: true,
-    },
-    {
-      field: "leads",
-      type: "number",
-      headerName: "Leads",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 130,
-      editable: true,
-    },
-    {
-      field: "affiliates",
-      headerName: "Affiliates",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 150,
-      editable: true,
-      sortable: false,
-    },
-    {
-      field: "price",
-      type: "number",
-      headerName: "Price",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 130,
-      editable: true,
-    },
-    {
-      field: "spend",
-      type: "number",
-      headerName: "Spend",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 130,
-      editable: true,
-    },
-    {
-      field: "profit",
-      type: "number",
-      headerName: "Profit",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 130,
-      valueGetter: (params: GridValueGetterParams) =>
-        (params.getValue(params.id, "price") || 0) -
-        params.getValue(params.id, "spend"),
-    },
-    {
-      field: "profitMargin",
-      headerName: "Profit %",
-      resizable: true,
-      headerAlign: "left",
-      align: "right",
-      width: 150,
-      valueGetter: (params: GridValueGetterParams) =>
-        Math.round(
-          ((params.getValue(params.id, "profit") || 0) /
-            (params.getValue(params.id, "price") || 0)) *
-            100
-        ),
-    },
-  ];
+  const { rows, headersMap } = useCampaigns();
   return (
     <Layout>
-      <div className="data-grid-container">
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={15}
-          checkboxSelection
-          disableSelectionOnClick
-        />
-      </div>
+      <EnhancedTable
+        rows={rows}
+        headersMap={headersMap}
+        Body={CampaignsTableBody}
+      />
     </Layout>
   );
 }
